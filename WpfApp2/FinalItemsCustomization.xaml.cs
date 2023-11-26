ed16306f944ed7b2c9a72db6e9fcb746e4f1bcae
danchan903 <danchan@ksu.edu>
2023-11-26 15:59:40 -0800

    added using statements

--- WpfApp2/FinalItemsCustomization.xaml.cs
+++ WpfApp2/FinalItemsCustomization.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WpfApp2
 {
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WpfApp2
 {
